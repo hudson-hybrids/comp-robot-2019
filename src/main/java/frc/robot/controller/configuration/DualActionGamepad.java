a2c2503b1e5a5da60534d7974498bf5c5fa476b4
Matthew Taylor <MatthewAlanTaylor@outlook.com>
2019-05-19 16:54:34 -0400

    Add controller-specific classes for customizability

---
+++ src/main/java/frc/robot/controller/configuration/DualActionGamepad.java
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.controller.configuration;
+
+public class DualActionGamepad {
+    public static final int A = 0;
+    public static final int B = 0;
+    public static final int X = 0;
+    public static final int Y = 0;
+    public static final int LEFT_SHOULDER = 0;
+    public static final int RIGHT_SHOULDER = 0;
+    public static final int START = 0;
+    public static final int SELECT = 0;
+    public static final int LEFT_TRIGGER = 0;
+    public static final int RIGHT_TRIGGER = 0;
+    public static final int LEFT_STICK = 0;
+    public static final int RIGHT_STICK = 0;
+}
---
+++ src/main/java/frc/robot/controller/configuration/Extreme3DProJoystick.java
@@ -0,0 +1,25 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.controller.configuration;
+
+public class Extreme3DProJoystick {
+    public static final int TOP_LEFT_BASE = 0;
+    public static final int TOP_RIGHT_BASE = 0;
+    public static final int MIDDLE_LEFT_BASE = 0;
+    public static final int MIDDLE_RIGHT_BASE = 0;
+    public static final int BOTTOM_LEFT_BASE = 0;
+    public static final int BOTTOM_RIGHT_BASE = 0;
+    public static final int TOP_LEFT_MAIN_STICK = 0;
+    public static final int TOP_RIGHT_MAIN_STICK = 0;
+    public static final int BOTTOM_LEFT_MAIN_STICK = 0;
+    public static final int BOTTOM_RIGHT_MAIN_STICK = 0;
+    public static final int TRIGGER = 0;
+    public static final int MAIN_STICK_SIDE = 0;
+    public static final int MAIN_STICK = 0;
+    public static final int THROTTLE_STICK = 0;
+}
